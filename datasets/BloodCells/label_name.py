9f9f426030014f0f19d4c18e7fa169d65e1fb9d4
cdl233 <1975684504@qq.com>
2024-05-08 14:49:23 +0800

    first commit

---
+++ datasets/BloodCells/label_name.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@Auth ： 逗逗班学Python
+博客园、知乎：逗逗班学Python
+Bilibili：逗逗班学Python
+面包多: 逗逗班学Python
+代码地址见以下博客中给出:
+https://space.bilibili.com/456667721
+https://www.cnblogs.com/deeppython
+
+@IDE ：PyCharm
+"""
+Chinese_name = {"Basophil": "嗜碱细胞", "Echinocytes": "刺状红细胞", "Elliptocyte": "椭圆形红细胞",
+                "Eosinophil": "嗜酸细胞",
+                "Erythroblast": "红细胞前体", "Hypochromia": "低色素症", "Lymphocyte": "淋巴细胞",
+                "Macrocyte": "大细胞",
+                "Microcyte": "小细胞", "Monocyte": "单核细胞", "Neutrophil": "中性粒细胞", "Ovalocyte": "椭圆细胞",
+                "Platelet": "血小板", "RBC": "红细胞", "Schistocyte": "裂片细胞", "Spherocyte": "球形细胞",
+                "Stomatocyte": "口形细胞", "TargetCell": "靶细胞", "Teardrop": "泪滴细胞", "WBC": "白细胞"}
+Label_list = list(Chinese_name.values())
